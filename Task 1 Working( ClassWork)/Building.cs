baec4e09dbe539bd8fc56e6a088a7e57ccd0d3b3
ModestRSA <56158279+ModestRSA@users.noreply.github.com>
2019-10-07 13:29:23 +0200

    Save/Load
    Fixed the save and load function in the form

---
+++ Task 1 Working( ClassWork)/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_Kevin_Kramer
+{
+    [Serializable]
+    public abstract class Building
+    {
+        protected int xPos;
+        protected int yPos;
+        protected int health;
+        protected int maxHealth;
+        protected int faction;
+        protected string symbol;
+
+        public abstract void Destruction();
+        public abstract override string ToString();
+    }
+}
